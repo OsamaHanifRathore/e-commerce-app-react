acf46504b0c6eed41b390b856337e6454ac82cc8
OsamaHanifRathore <48179164+OsamaHanifRathore@users.noreply.github.com>
2020-08-24 12:21:35 +0500

    updated our app collection preview item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch , Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.components';
 
 const HatsPage =()=>(
 <div>
@@ -18,7 +19,7 @@ function App() {
     <div className="App">   
      <Switch>
       <Route exact path = '/' component={HomePage}/>
-      <Route path ='/hats' component ={HatsPage}/>
+      <Route path ='/shop' component ={ShopPage}/>
      </Switch>
     </div>
  
